831683b0f3ba07f270fc5a1adc53c9179f20cc94
riotbeck <32631862+riotbeck@users.noreply.github.com>
2018-01-14 22:25:00 +0100

    Add files via upload

--- database/analysis/main.py
+++ database/analysis/main.py
@@ -1,30 +1,52 @@
 import MySQLdb
+from time import sleep
 
 # Datenbankverbindung
 connection = MySQLdb.connect("localhost", "root", "hamburg", "smartkitchen")
 cursor = connection.cursor()
 
-# Aktuellen NFC-Tag und die vier letzten Gewichtswerte auswaehlen
-cursor.execute("SELECT data FROM nfc ORDER BY timestamp DESC LIMIT 1")
-nfc_raw = cursor.fetchone()
-cursor.execute("SELECT data FROM sensor ORDER BY timestamp DESC LIMIT 4")
-fsr_raw = cursor.fetchmany(size=4)
-weight = (fsr_raw[0] + fsr_raw[1] + fsr_raw[2] + fsr_raw[3]) / 4
+old_id = 0
+while (1):
+    sleep(1)
+    print("looking for changes")
+    cursor.execute("SELECT id FROM nfc ORDER BY timestamp DESC LIMIT 1;")
+    id = cursor.fetchone()
+    if (id != old_id):
+        print("Something changed")
+        old_id = id
+        # Aktuellen NFC-Tag und die vier letzten Gewichtswerte auswaehlen
+        cursor.execute("SELECT data FROM nfc ORDER BY timestamp DESC LIMIT 1;")
+        nfc_tag = cursor.fetchone()[0]
+        cursor.execute("SELECT wert FROM sensor ORDER BY timestamp DESC LIMIT 1;")
+        fsr_data = cursor.fetchone()[0]
+        a, b, c, d = fsr_data.split(",")
 
-# Debug
-print(nfc_raw)
-print(fsr_raw)
-print(weight)
+        weight = (int(a) + int(b) + int(c) + int(d)) / 4
 
-# ID mit NFC-Tag matchen
-cursor.execute("SELECT ID FROM xxx WHERE nfc_tag=" + nfc_raw)
-actual_id = cursor.fetchone()
+        print(nfc_tag)
+        print(weight)
 
-# ID mit LaufenderID matchen
-cursor.execute("SELECT LID FROM xxx WHERE ID=" + actual_id)
-actual_Lid = cursor.fetchone()
-# Bestand aktuallisieren
-if(actual_Lid == -1):
-    cursor.execute("INSERT INTO xxx (ID, actual_force, temp) VALUES (" + actual_id + ", " + weight + " ,1)")
-else:
-    cursor.execute("DELETE FROM xxx WHERE ID=" + actual_id)
+        cursor.execute("SELECT id FROM produktmatch WHERE nfctag=" + "\"" + nfc_tag + "\"" + ";")
+        actual_id = cursor.fetchone()[0]
+        print(actual_id)
+
+        cursor.execute("SELECT produkt FROM produktmatch WHERE id=" + str(actual_id) + ";")
+        actual_produkt = cursor.fetchone()[0]
+        cursor.execute("SELECT kategorie FROM produktmatch WHERE id=" + str(actual_id) + ";")
+        actual_kategorie = cursor.fetchone()[0]
+
+        cursor.execute("SELECT id FROM bestand WHERE id=" + str(actual_id) + ";")
+        actual_Lid_raw = cursor.fetchone()
+        print(actual_Lid_raw)
+        if (actual_Lid_raw == None):
+            str1 = "INSERT INTO bestand (id, gewicht, temp, kategorie, produkt) VALUES (" + str(actual_id) + "," + str(
+                int(weight)) + ",1," + "\"" + str(actual_kategorie) + "\",\"" + str(actual_produkt) + "\");"
+            cursor.execute(str1)
+            connection.commit()
+            print("Data written")
+        else:
+            print("DELETE Data")
+            actual_Lid = list(actual_Lid_raw)
+            print(actual_Lid[0])
+            cursor.execute("DELETE FROM bestand WHERE id=" + str(actual_id) + ";")
+            connection.commit()
